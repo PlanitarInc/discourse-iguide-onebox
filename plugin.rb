0e5f563bbf0bdce65aa67c688c0819d0c28cb748
Sasha <alex@planitar.com>
2017-03-27 22:43:43 -0400

    initial tests

--- plugin.rb
+++ plugin.rb
@@ -10,16 +10,6 @@ register_css <<CSS
 .youriguide-embed {
     position: relative;
     height: 400px;
-    border: 1px solid #ddd;
-    border-radius: 3px;
-}
-
-.youriguide-container {
-    position: absolute;
-    top: 33px;
-    bottom: 1px;
-    left: 1px;
-    right: 1px;
 }
 
 .youriguide-frame {
@@ -38,28 +28,18 @@ class Onebox::Engine::youriguideOnebox
         0
     end
 
-    REGEX = /^https?:\/\/(www\.)?youriguide\.com\/([0-9a-z\-]+)$/
+    REGEX = /^https?:\/\/(?:www\.)?youriguide\.com\/(?:embed\/)?(.*?)$/i
     
-    REGEX = /^https?:\/\/pseuco\.com\/\#(?:\/sku\/\w+\/\w+)?\/edit\/remote\/([0-9a-z\-]+)$/
-    
-    /^(https?:)?\/\/(www\.)?youriguide\.com\/.+/i
-
     matches_regexp REGEX
 
     def id
         @url.match(REGEX)[1]
     end
 
-    def url
-        "https://youriguide.com/{id}"
-    end
-
     def to_html
         <<HTML
         <div class="youriguide-embed">
-            <div class="youriguide-container">
-                <iframe class="youriguide-frame" src="//youriguide.com/{id}"></iframe>
-            </div>
+                <iframe class="youriguide-frame" src="//youriguide.com/#{id}"></iframe>
         </div>
 HTML
     end
